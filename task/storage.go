9f3a43fa6c13a2a1d8116e2be540ede2c742e668
Adilfarooque <Adilfarq11@gmail.com>
2024-09-14 12:24:17 +0530

    filepath

--- task/storage.go
+++ task/storage.go
@@ -1,1 +1,35 @@
 package task
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const filePath = "./tasks.json"
+
+func LoadTasks() ([]Task, error) {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return []Task{}, nil
+	}
+
+	file, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var tasks []Task
+	err = json.Unmarshal(file,&tasks)
+	if err != nil{
+		return nil,err
+	}
+	return tasks,nil
+}
+
+func SaveTasks(tasks []Task)error{
+	data,err := json.MarshalIndent(tasks,"","  ")
+	if err != nil{
+		return err
+	}
+
+	return os.WriteFile(filePath,data,0644)
+}
